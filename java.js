2343542611d003e03e95849a3dec2e33c0f53927
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-01-03 13:18:53 +0600

    add my card sextion

--- java.js
+++ java.js
@@ -5,4 +5,46 @@ const useData = async (searchText='12') => {
     displayData(response.data);
   }
   
+  const displayData = (data) => {
+    console.log(data);
+    // step 1 ,Find the div
+    const findDivContainer = document.getElementById('find-the-div');
+    // clear phone container cards before the search 
+    findDivContainer.innerHTML = '';
+  // show all button 
+     const showAllButton = document.getElementById('show-all-container');
+    if (data.length > 9) {
+      showAllButton.classList.remove('hidden');
+    }
+     else {
+      showAllButton.classList.add('hidden');
+      }
   
+  
+    // limit of your phone 
+    data = data.slice(0, 10);
+    data.forEach(phone => {
+      console.log(phone);
+      // step 2 create the div
+      const createTheDiv = document.createElement('div');
+      // step 3 give them input 
+      createTheDiv.classList = `card  bg-base-100 shadow-xl`;
+      createTheDiv.innerHTML =`
+      <figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <div class="card-body">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <p>${phone.slug}</p>
+              <div class="card-actions justify-center">
+                <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+              </div>
+  
+            </div>`
+      // append the child 
+      findDivContainer.appendChild(createTheDiv);
+      
+      
+    });
+    addLoading(false);
+    
+    
+  }
